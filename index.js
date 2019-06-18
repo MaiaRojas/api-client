950b5e081e09e06a10579f848b55f2751297d347
Fabián Alexis Bravo Abarca <fabian.bravo.abarca@gmail.com>
2019-06-18 11:49:59 -0400

    WIP solving circular dependencies

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 import APIClient from './lib/APIClient';
+import reducers from './lib/reducers';
+import actions from './lib/actions';
+
+APIClient.getAPIReducers = () => reducers;
+APIClient.getAPIActions = () => actions;
 
 export default APIClient;
