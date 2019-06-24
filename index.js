ebaea05588bb3e0d34177b3283db7eb150538b24
Fabián Alexis Bravo Abarca <fabian.bravo.abarca@gmail.com>
2019-06-24 11:52:32 -0400

    fix(api): calls using get or delete now doesn't trigger errors because they have a body

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 import APIClient from './lib/APIClient';
-import reducers from './lib/reducers';
 import action from './lib/actions';
 
-APIClient.getAPIReducers = () => reducers;
 APIClient.doAPICall = action;
 
 export default APIClient;
