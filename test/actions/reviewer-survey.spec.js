3b159accd4b2efd039bcd414886748fc3809fcaf
MaiaRojas <maia.rt.46@gmail.com>
2019-08-28 14:14:11 -0500

    chore(reviewer-survey-test): Add test for reviewer-survey actions

---
+++ test/actions/reviewer-survey.spec.js
@@ -0,0 +1,21 @@
+import { getLastestVersion, getReviewerSurvey } from '../../lib/actions/reviewer-survey';
+
+describe('getLastestVersion', () => {
+  it('should be a function', () => {
+    expect(typeof getLastestVersion).toBe('function');
+  });
+
+  it('should create an appropriate action', () => {
+    expect(getLastestVersion()).toMatchSnapshot();
+  });
+});
+
+describe('getReviewerSurvey', () => {
+  it('should be a function', () => {
+    expect(typeof getReviewerSurvey).toBe('function');
+  });
+
+  it('should create an appropriate action', () => {
+    expect(getReviewerSurvey({ version: 'something' })).toMatchSnapshot();
+  });
+});
