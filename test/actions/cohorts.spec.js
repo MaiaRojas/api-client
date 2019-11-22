dee45f56e6cb2158e6d1039c92b3ea79fde4d22b
MaiaRojas <maia.rt.46@gmail.com>
2019-11-22 12:35:00 -0500

    chore(cohort): Add tests for getCohortUsers and getCohortProjects action

--- test/actions/cohorts.spec.js
+++ test/actions/cohorts.spec.js
@@ -1,11 +1,16 @@
-import { getCohorts, getCohortUsers } from '../../lib/actions/cohorts';
+import {
+  getCohorts,
+  getCohortUsers,
+  getCohortProjects,
+  addCohortProject,
+} from '../../lib/actions/cohorts';
 
 describe('getCohorts', () => {
   it('should be a function', () => {
     expect(typeof getCohorts).toBe('function');
   });
 
-  it('should create an appropriate action', () => {
+  it('should create an action', () => {
     expect(getCohorts()).toMatchSnapshot();
   });
 });
@@ -15,7 +20,32 @@ describe('getCohortUsers', () => {
     expect(typeof getCohortUsers).toBe('function');
   });
 
-  it('should create an appropriate action', () => {
+  it('should create an action', () => {
     expect(getCohortUsers({ cohort: 'something' })).toMatchSnapshot();
+  });
+});
+
+describe('getCohortProjects', () => {
+  it('should be a function', () => {
+    expect(typeof getCohortProjects).toBe('function');
+  });
+
+  it('should create an action', () => {
+    expect(getCohortProjects({ cohort: 'someone' })).toMatchSnapshot();
+  });
+});
+
+describe('addCohortProject', () => {
+  it('should be a function', () => {
+    expect(typeof addCohortProject).toBe('function');
+  });
+
+  it('should create an action', () => {
+    expect(addCohortProject({
+      cohort: 'someone',
+      project: {
+        id: 'projectId',
+      },
+    })).toMatchSnapshot();
   });
 });
